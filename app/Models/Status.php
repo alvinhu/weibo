a6251ca6ae3c1b3254aa8798bed2587e391e8282
Zhuolei Hu <huzhuolei@qq.com>
2018-12-20 16:28:26 +0000

    用户微博列表

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    return $this->belongsTo(User::class);
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,11 +5,6 @@ use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         $users = factory(User::class)->times(50)->make();
